274d76faf65a4aeee33abda4c4b1b97c2b515695
pyingzhu <peiying.zhu@ehl.ch>
2026-06-06 17:03:22 +0800

    chore(edgepro): scaffold app config files

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const path = require('path')
+
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'standalone',
+  outputFileTracingRoot: path.join(__dirname, '../../'),
+}
+
+module.exports = nextConfig
